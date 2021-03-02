82d2f532518794db4c095b3ac5fb5e1f98ad9bf4
Otabek Samatov <otabeksamatov11235@gmail.com>
2021-03-02 11:50:32 -0600

    +

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ region = var.region
  terraform {
   required_version  =   "~> 0.12.23"
   required_providers {
-    aws      =   "~> 2.53.0"
+    aws      =   "~> 3.0.0"
     null     =   "2.1.2"
     local    =   "1.4.0"
     template =   "2.1.2"
